ad393ee7e6c0239a7d3a5f257c717b22c3354958
Eesha Srivastava <65655828+eeshasrivastava@users.noreply.github.com>
2021-02-14 21:29:31 +0530

    Updating the file

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ using randrange function
 '''
 start = 1
 end = 10000
-num = random.randrange(start,end+1)
+num = random.randrange(start, end+1)
 
 #Now choosing the first digit of num from left...
 
@@ -17,12 +17,13 @@ num_2 = int(num_2)
 Finally, choosing the number for the die
 '''
 if num_2 < 6:
-    roll = random.randrange(1,num_2+1)
+    roll = random.randrange(1, num_2+1)
 elif num_2 == 6:
     roll = num_2
+
 else:
     num_2 = num_2 - 3
-    roll = random.randrange(1,num_2+1)
+    roll = random.randrange(1, num_2+1)
 
-print("You got",roll)
+print("You got", roll)
 input("Press Enter to exit.")
